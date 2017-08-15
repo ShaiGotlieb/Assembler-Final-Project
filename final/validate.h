b046afa738cdab77287a78e505d863c4abd6d3d1
ShaiGotlieb <30575967+ShaiGotlieb@users.noreply.github.com>
2017-08-15 20:25:39 +0300

    Update validate.h

--- final/validate.h
+++ final/validate.h
@@ -1,14 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
- 
+#ifndef _VALIDATE_H
+#define _VALIDATE_H
+
 #define IMMEDIATE_ADRESS 0
 #define DIRECT_ADRESS 1
 #define MAT_ADRESS 2
 #define DIRECT_REG 3
 #define MAX_CMD 16
 #define MAX_OPERAND 3
+
+char r0[4] = "0000";
+char r1[4] = "0001";
+char r2[4] = "0010";
+char r3[4] = "0011";
+char r4[4] = "0100";
+char r5[4] = "0101";
+char r6[4] = "0110";
+char r7[4] = "0111";
  
  
 int typeAdress(splitLines*, int);
@@ -32,3 +39,5 @@ int validEntry(char*, Symblist*);
 int validString(char*);
 int memorySize(splitLines*);
 int isVar(char*);
+
+#endif
